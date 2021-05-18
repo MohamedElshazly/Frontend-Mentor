60413d6e37b1af985e7a5aa3ecb3d8f29dd748cc
Mohamed <mohamedelshazly18@yahoo.com>
2021-05-18 22:37:23 +0200

    stats-card

---
+++ stats-preview-card/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {
+        colors: {
+          blue: {
+            midnight : '#1c1938',
+          },
+          black: {
+            lighter: '#0a0c1b',
+          },
+        }
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
